67279aec3cd386a0191316a8273091e39496ac1b
George Kamtziridis <georgekam96@gmail.com>
2023-03-06 20:24:38 +0200

    Added basic numpy array encoder.

---
+++ NumpyArrayEncoder.py
@@ -0,0 +1,17 @@
+import json
+import numpy as np
+
+
+class NumpyArrayEncoder(json.JSONEncoder):
+    """
+    Handles the encoding of numpy values to JSON values.
+    """
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(NumpyArrayEncoder, self).default(obj)
